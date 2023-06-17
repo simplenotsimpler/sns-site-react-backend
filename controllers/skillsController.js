7c8428e6e7dcee2c056e04f897639a297ffcf617
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-06-17 18:16:37 -0400

    feature: get skills

---
+++ controllers/skillsController.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+
+const getSkills = async (req, res, next) => {
+  try {
+    const api_key = process.env.MONGO_DATA_API_KEY;
+    let url = process.env.MONGO_DATA_BASE_URI;
+
+    url = `${url}/skills`;
+
+    let config = {
+      method: "get",
+      url: `${url}`,
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "api-key": `${api_key}`,
+      },
+    };
+
+    const axiosResponse = await axios(config);
+    console.log();
+
+    const skills = axiosResponse.data;
+
+    res.json({ skills });
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getSkills };
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const projectsController = require("../controllers/projectsController.js");
+const skillsController = require("../controllers/skillsController.js");
 
 router.route("/", (req, res) => {
-  res.send('API is running....');
+  res.send("API is running....");
 });
 
 router.route("/projects").get(projectsController.getProjects);
+
+router.route("/skills").get(skillsController.getSkills);
+
 module.exports = router;
