889dc1b5528b6a747f1aadec3d1adafcd4ba2fce
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-07-03 11:15:01 -0400

    fix: rate limiter errors

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const hpp = require("hpp");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions.js");
 
+const rateLimiter = require("./middleware/rateLimiter.js");
+
 const { logger } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler.js");
 const PORT = process.env.PORT || 5000;
