835f878b9a8de440a6b09f6796f1caab62c42885
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-07-13 20:42:26 -0400

    fix(ProjectsController): tweak dates

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -13,7 +13,7 @@ const getProjects = async (req, res, next) => {
 
     const ghQueryJSON = `
     {
-      "query": "query {viewer { pinnedItems(first: 10) {repos: nodes { ... on Repository { name description url homepageUrl openGraphImageUrl stargazerCount createdAt updatedAt forks {totalCount} watchers { totalCount } repositoryTopics(first: 20) { nodes { topic { name } } } languages(first: 20, orderBy: {field: SIZE, direction: DESC}) { languageList:edges {language: node { name } } } } } } } }"
+      "query": "query {viewer { pinnedItems(first: 10) {repos: nodes { ... on Repository { name description url homepageUrl openGraphImageUrl stargazerCount createdAt updatedAt pushedAt forks {totalCount} watchers { totalCount } repositoryTopics(first: 20) { nodes { topic { name } } } languages(first: 20, orderBy: {field: SIZE, direction: DESC}) { languageList:edges {language: node { name } } } } } } } }"
     }
     `;
 
@@ -38,10 +38,26 @@ const getProjects = async (req, res, next) => {
       },
     } = axiosResponse.data;
 
-    //sort pinnedRepos by stargazerCount
-    projects.sort((a, b) => b.stargazerCount - a.stargazerCount);
+    //brickyard dates in GitHub not accurate since the repo is a container for the site screenshot and basic info. dates are approximate
+    //brickyard around time of modern-table 1.0.6 release
+    projects.find(
+      (project) => project.name === "brickyard-ceramics"
+    ).createdAt = "2020-06-15";
 
-    res.json(projects);
+    //last modified per the headers.
+    projects.find((project) => project.name === "brickyard-ceramics").pushedAt =
+      "2021-10-08";
+
+    //sort pinnedRepos by stargazerCount
+    // projects.sort((a, b) => b.stargazerCount - a.stargazerCount);
+
+    //sort by pushedAt
+    const sortedProjects = projects
+      .slice()
+      .sort((a, b) => new Date(b.pushedAt) - new Date(a.pushedAt));
+
+    res.json(sortedProjects);
+
   } catch (error) {
     next(error);
   }
